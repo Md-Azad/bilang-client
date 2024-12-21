e570bc9bd2cc81cc7620062b0cb6882e9170c29f
Md Azad Hossain <sokalazad@gmail.com>
2024-12-22 00:36:12 +0100

    demo language category section added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "./Banner";
+import LanguageCategory from "./LanguageCategory";
 import Stats from "./Stats";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
     <div>
       <Banner></Banner>
       <Stats></Stats>
+      <LanguageCategory></LanguageCategory>
     </div>
   );
 };
---
+++ src/pages/Home/LanguageCategory.jsx
@@ -0,0 +1,72 @@
+import logoImg from "../../assets/logo.webp";
+const LanguageCategory = () => {
+  return (
+    <section className="mx-2 grid md:grid-cols-3 gap-4 ">
+      <div className="flex gap-4 justify-between items-center border-2 border-gray-400 pr-4 rounded-md">
+        <div className="flex gap-4 items-center pl-4 py-4">
+          <img className="w-8 h-8 rounded-md" src={logoImg} alt="" />
+          <h1 className="text-3xl text-gray-500 font-bold">English Tutors</h1>
+        </div>
+        <p> &gt; </p>
+      </div>
+      <div className="flex gap-4 justify-between items-center border-2 border-gray-400 pr-4 rounded-md">
+        <div className="flex gap-4 items-center pl-4 py-4">
+          <img className="w-8 h-8 rounded-md" src={logoImg} alt="" />
+          <h1 className="text-3xl text-gray-500 font-bold">English Tutors</h1>
+        </div>
+        <p> &gt; </p>
+      </div>
+      <div className="flex gap-4 justify-between items-center border-2 border-gray-400 pr-4 rounded-md">
+        <div className="flex gap-4 items-center pl-4 py-4">
+          <img className="w-8 h-8 rounded-md" src={logoImg} alt="" />
+          <h1 className="text-3xl text-gray-500 font-bold">English Tutors</h1>
+        </div>
+        <p> &gt; </p>
+      </div>
+      <div className="flex gap-4 justify-between items-center border-2 border-gray-400 pr-4 rounded-md">
+        <div className="flex gap-4 items-center pl-4 py-4">
+          <img className="w-8 h-8 rounded-md" src={logoImg} alt="" />
+          <h1 className="text-3xl text-gray-500 font-bold">English Tutors</h1>
+        </div>
+        <p> &gt; </p>
+      </div>
+      <div className="flex gap-4 justify-between items-center border-2 border-gray-400 pr-4 rounded-md">
+        <div className="flex gap-4 items-center pl-4 py-4">
+          <img className="w-8 h-8 rounded-md" src={logoImg} alt="" />
+          <h1 className="text-3xl text-gray-500 font-bold">English Tutors</h1>
+        </div>
+        <p> &gt; </p>
+      </div>
+      <div className="flex gap-4 justify-between items-center border-2 border-gray-400 pr-4 rounded-md">
+        <div className="flex gap-4 items-center pl-4 py-4">
+          <img className="w-8 h-8 rounded-md" src={logoImg} alt="" />
+          <h1 className="text-3xl text-gray-500 font-bold">English Tutors</h1>
+        </div>
+        <p> &gt; </p>
+      </div>
+      <div className="flex gap-4 justify-between items-center border-2 border-gray-400 pr-4 rounded-md">
+        <div className="flex gap-4 items-center pl-4 py-4">
+          <img className="w-8 h-8 rounded-md" src={logoImg} alt="" />
+          <h1 className="text-3xl text-gray-500 font-bold">English Tutors</h1>
+        </div>
+        <p> &gt; </p>
+      </div>
+      <div className="flex gap-4 justify-between items-center border-2 border-gray-400 pr-4 rounded-md">
+        <div className="flex gap-4 items-center pl-4 py-4">
+          <img className="w-8 h-8 rounded-md" src={logoImg} alt="" />
+          <h1 className="text-3xl text-gray-500 font-bold">English Tutors</h1>
+        </div>
+        <p> &gt; </p>
+      </div>
+      <div className="flex gap-4 justify-between items-center border-2 border-gray-400 pr-4 rounded-md">
+        <div className="flex gap-4 items-center pl-4 py-4">
+          <img className="w-8 h-8 rounded-md" src={logoImg} alt="" />
+          <h1 className="text-3xl text-gray-500 font-bold">English Tutors</h1>
+        </div>
+        <p> &gt; </p>
+      </div>
+    </section>
+  );
+};
+
+export default LanguageCategory;
