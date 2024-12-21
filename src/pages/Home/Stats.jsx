70eea21457f5e83e4c083f3cb60166d128b64ec7
Md Azad Hossain <sokalazad@gmail.com>
2024-12-22 00:14:37 +0100

    added stats section

--- src/pages/Home/Stats.jsx
+++ src/pages/Home/Stats.jsx
@@ -1,7 +1,35 @@
+import { IoIosStar } from "react-icons/io";
 const Stats = () => {
   return (
-    <div>
-      <h1>Stats will be here.</h1>
+    <div className="stats stats-vertical lg:stats-horizontal shadow w-full my-4 shadow-black">
+      <div className="stat">
+        <div className="stat-value">32,000+</div>
+        <div className="stat-desc">Experience tutors</div>
+      </div>
+
+      <div className="stat">
+        <div className="stat-value">30,000+</div>
+        <div className="stat-desc">5-Star tutor reviews</div>
+      </div>
+      <div className="stat">
+        <div className="stat-value">120</div>
+        <div className="stat-desc">Subjects taught</div>
+      </div>
+
+      <div className="stat">
+        <div className="stat-value">180+</div>
+        <div className="stat-desc">Tutor Nationalities</div>
+      </div>
+      <div className="stat">
+        <div className="stat-value flex items-center">
+          4.8 <IoIosStar />
+          <IoIosStar />
+          <IoIosStar />
+          <IoIosStar />
+          <IoIosStar />
+        </div>
+        <div className="stat-desc text-center">on the App Store</div>
+      </div>
     </div>
   );
 };
