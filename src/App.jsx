77fba3ffbee9690659f177aeff811956420084c1
Md Azad Hossain <sokalazad@gmail.com>
2024-12-21 22:06:45 +0100

    basic project setup with react router dom, tailwind and daisy UI

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-900">BiLang</h1>
+      <button className="btn btn-accent">Accent</button>
+    </>
+  );
+}
+
+export default App;
