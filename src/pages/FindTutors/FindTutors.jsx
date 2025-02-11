3f1f3d2e45f01b2946633d8a8040d95f71191419
Md Azad Hossain <sokalazad@gmail.com>
2025-02-11 20:37:16 +0100

    added sorting according price of the session

--- src/pages/FindTutors/FindTutors.jsx
+++ src/pages/FindTutors/FindTutors.jsx
@@ -1,11 +1,27 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TutorCard from "./TutorCard";
 import Search from "./Search";
 
 const FindTutors = () => {
   const [tutors, setTutors] = useState([]);
   const [search, setSearch] = useState("");
+  const [filteredTutors, setFilderedTutors] = useState([]);
+
+  useEffect(() => {
+    if (tutors.length > 0) {
+      setFilderedTutors(tutors);
+    }
+  }, [tutors]);
+
+  const handleMinSort = () => {
+    const sortedPrice = [...tutors].sort((a, b) => a.price - b.price);
+    setFilderedTutors(sortedPrice);
+  };
+  const handleMaxSort = () => {
+    const sortedPrice = [...tutors].sort((a, b) => b.price - a.price);
+    setFilderedTutors(sortedPrice);
+  };
 
   useState(() => {
     axios
@@ -25,13 +41,18 @@ const FindTutors = () => {
   return (
     <div>
       <div className="flex justify-between">
-        <h1 className="text-3xl text-center text-green-600">
-          Find Your Tutor Here
-        </h1>
+        <div className="flex justify-end gap-4 ">
+          <button onClick={handleMinSort} className="btn btn-info text-white">
+            Sort By Min Price
+          </button>
+          <button onClick={handleMaxSort} className="btn btn-accent text-white">
+            Sort By Max Price
+          </button>
+        </div>
         <Search setSearch={setSearch}></Search>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-4 ">
-        {tutors
+        {filteredTutors
           .filter((item) => {
             return search.toLowerCase() === ""
               ? item
